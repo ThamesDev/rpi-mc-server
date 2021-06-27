76583a4818382ffd798ea7db778bdd195932dc31
ThamesDev <freddie@jemison.co.uk>
2021-06-27 18:23:27 +0100

    Update `.sh` files

--- install-server.sh
+++ install-server.sh
@@ -1,5 +1,6 @@
 cd $HOME
 mkdir MinecraftServer
+cd MinecraftServer/
 wget https://launcher.mojang.com/v1/objects/0a269b5f2c5b93b1712d0f5dc43b6182b9ab254e/server.jar
 wget https://github.com/AdoptOpenJDK/openjdk16-binaries/releases/download/jdk-16.0.1%2B9/OpenJDK16U-jre_arm_linux_hotspot_16.0.1_9.tar.gz
 tar -xvzf OpenJDK16U-jre_arm_linux_hotspot_16.0.1_9.tar.gz
